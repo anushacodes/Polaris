442bb29fa4fbe1cde3fb93670c607bc3b72d1e6a
stonklord84 <johnnywang05292005@gmail.com>
2026-06-16 00:34:28 -0400

    neighborhood retrieval logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,32 @@
-import { query } from '@/lib/db';
+"use client";
 
-export default async function Page() {
-  const response = await query('select * from comments');
-  const comments = response.rows;
+import { useActionState } from "react";
+import { retrieveNeighborhoods } from "./actions";
+
+export default function Prototype() {
+  const [data, formAction, isPending] = useActionState(retrieveNeighborhoods, null);
 
   return (
-    <main className="main">
-      <h1 className="title">Next.js + Aurora PostgreSQL</h1>
-
-      <div className="container">
-        {comments.map(comment => (
-          <div>
-            <p>{comment.comment}</p>
-            <div className="metadata">{comment.id}</div>
-          </div>
-         ))}
-      </div>
-    </main>
+    <div>
+      <form action={formAction}>
+        <textarea name="userPrompt" placeholder="describe your preferences..." required />
+        <button type="submit" disabled={isPending}>
+          {isPending ? "Loading..." : "Submit"}
+        </button>
+      </form>
+      
+      {data && data.map((info: any, idx: number) =>(
+        <div key={idx}>
+          <p>{info.name}</p>
+          <p>{info.city}, {info.country}</p>
+          <p>Safety: {info.safety_score}</p>
+          <p>Transit: {info.transit_score}</p>
+          <p>Vibe: {info.vibe_score}</p>
+          <p>{info.description}</p>
+          <p>Tags: {info.tags?.join(", ")}</p>
+          <p>Rent: {info.min_rent} - {info.max_rent}</p><br />
+        </div>
+      ))}
+    </div>
   );
 }
